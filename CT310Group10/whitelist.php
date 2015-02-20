4a636dc432b588973b29235e78fed260fd056bc9
davsay <david.sahud@gmail.com>
2015-02-20 12:36:33 -0700

    Update whitelist.php
    Now strips html tags and only allows ip 192.82 to update

--- CT310Group10/whitelist.php
+++ CT310Group10/whitelist.php
@@ -8,9 +8,6 @@ This may be included into page1 and page2 later
 
 Todo: 
 
--Strip html tags when inputing new text
--Only allow 192.82 ip addresses to edit
--edit button?
 
 
 -->
@@ -24,15 +21,31 @@ Todo:
 		$ipArray = explode(".",$ip);
 		print_r($ipArray);
 		
-		
-		if($ipArray[0] = 192){
+		$edit = 0;
+		if($ipArray[0] == 192){
 			echo $ipArray[0];
-			if($ipArray[1] = 82){
+			if($ipArray[1] == 82){
 				echo $ipArray[1];
 				$edit = 1;
 				//if edit == 1, then can edit...
 			}
 		}
+		
+	//updating bio file
+	if (isset($_POST['update'])) {//update button has been hit
+		if($edit == 1){
+		$content = strip_tags($_POST['text_box']);
+		$oneText = "UserOneBio.txt";
+		$oneWrite = fopen($oneText,'w') or die("file not found");
+		$items = $content;
+		//used to fill text
+		//fwrite($oneWrite,"This contents is about user one and their life");
+		fwrite($oneWrite,$items);
+		fclose($oneWrite);
+		}else{//not allowed to edit
+			echo "Not allowed to update";	
+		}
+	}
 	//reading in file to put in text box	
 	$oneText = "UserOneBio.txt";
 	if(!file_exists($oneText)) touch($oneText); //if file doesn't exit make one
@@ -45,21 +58,10 @@ Todo:
 	}
 	fclose($oneOpen);
 	
-	//updating bio file
-	if (isset($_POST['update'])) {//update button has been hit
-	$content = strip_tags($_POST['text_box']);
-	$oneText = "UserOneBio.txt";
-	$oneWrite = fopen($oneText,'w') or die("file not found");
-	$items = $content;
-	//used to fill text
-	//fwrite($oneWrite,"This contents is about user one and their life");
-	fwrite($oneWrite,$items);
-	fclose($oneWrite);
-	}
 	?>
    <p>TextBox</p>
    <form method="post" action="whitelist.php">
-      Bio: <br><textarea name="text_box" rows="5" cols="40" placeholder="Enter Bio Info Here"><?php echo $fileContents; ?></textarea><br/>
+      Bio: <br><textarea name="text_box" rows="5" cols="40" placeholder="Enter Bio Info Here"><?php echo strip_tags($fileContents); ?></textarea><br/>
      <input type="submit" value="Update" name="update">
    </form>
 </body>

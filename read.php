0527a25ffc15eda39084156747d088a200f14b39
root <root@localhost.localdomain>
2015-02-22 16:17:32 -0700

    reading with new lines

--- read.php
+++ read.php
@@ -2,7 +2,7 @@
 <?php
 $myfile = fopen($file, "r") or die("Unable to open file!");
 if(filesize($file) > 0){
-echo fread($myfile,filesize($file));
+echo strip_tags(nl2br(fread($myfile,filesize($file))), '<br>');
 }
 fclose($myfile);
 ?>

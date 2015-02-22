e3a3b072284bf5a0a74936df5107205cb9b4712c
brandily <bmegrew@comcast.net>
2015-02-22 06:26:14 -0700

    set up search page

--- index.php
+++ index.php
@@ -12,15 +12,25 @@ include 'header.php';
 		<!-- site information - temp info -->
 		<h3>Site Information</h>
 			<div class="left-body">
-				<li>thing</li>
-				<li>thing</li>
-				<li>thing</li>
-				<li>thing</li>
-				<li>thing</li>
-				<li>thing</li>
-				<li>thing</li>
-				<li>thing</li>
-				<li>thing</li>
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+Vestibulum at consectetur ligula. Cras sodales nibh turpis, tristique viverra mi consectetur 
+sit amet. Donec sapien urna, convallis vel laoreet sit amet, rutrum eu nisi. Sed quis blandit 
+leo. Etiam accumsan id leo at malesuada. Proin porttitor sapien neque, in scelerisque sapien 
+congue sed. Suspendisse rhoncus odio quis sem laoreet, quis interdum leo consectetur. Duis 
+tristique sapien augue, sit amet commodo lacus tempus quis. Pellentesque eget maximus est, 
+vitae volutpat turpis. Sed ultrices augue eget ante elementum euismod. Vestibulum mollis arcu 
+pharetra, fermentum lorem feugiat, tristique felis.</p><br>
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+Vestibulum at consectetur ligula. Cras sodales nibh turpis, tristique viverra mi consectetur 
+sit amet. Donec sapien urna, convallis vel laoreet sit amet, rutrum eu nisi. Sed quis blandit 
+leo. Etiam accumsan id leo at malesuada. Proin porttitor sapien neque, in scelerisque sapien 
+congue sed. Suspendisse rhoncus odio quis sem laoreet, quis interdum leo consectetur. Duis 
+tristique sapien augue, sit amet commodo lacus tempus quis. Pellentesque eget maximus est, 
+vitae volutpat turpis. Sed ultrices augue eget ante elementum euismod. Vestibulum mollis arcu 
+pharetra, fermentum lorem feugiat, tristique felis.</p><br>
+
+
+
 			</div>
 		</div>
 		<div class="right-wrap">
@@ -30,13 +40,6 @@ include 'header.php';
 					<ul id="home-user-list">
 						<li><a href="user1.php"><img src="./img/user1.png" alt="A picture of Mario"></a>Mario<br><br></li>
 						<li><a href="user2.php"><img src="./img/user2.png" alt="A picture of Luigi"></a>Luigi<br></li>
-						<li>thing</li>
-						<li>thing</li>
-						<li>thing</li>
-						<li>thing</li>
-						<li>thing</li>
-						<li>thing</li>
-						<li>thing</li>
 					</ul>
 				</div>
 			</div>
--- search.php
+++ search.php
@@ -1,72 +1,36 @@
-<!-- homepage for social networking site 
-change ip's:
-129.82.*
+<?php
+$page = 'Search';
+include 'header.php';
+?>
 
--->
-<!--?xml version="1.0" encoding="utf-8"?-->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<style type="text/css">
+@import '/styles/search.css';	
+</style>
 
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-   		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Home Page</title>
-		<link rel="stylesheet" type="text/css" href="./styles/mystyle1.css" />
-	</head>
-	<body>
-		<!--
-		<?php
-			$ip = $_SERVER["REMOTE_ADDR"];
-			$ipArray = explode(".", $ip);
-			print_r($ipArray);
-		?>
-		-->
-		<div class="top-wrap">
-			<h1 id="header-label">Social Networking Platform</h1>
-			<h3>Search</h3>
-			<nav>
-				<ul>
-					<li><a href="./index.html" id="home-nav">Home</a></li>
-					<li><a href="" id="search-nav">Search</a></li>
-					<li><a href="" id="your-page-nav">Your Page</a></li>
-					<li><a href="" id="sign-in-nav">Sign In</a></li>
-				</ul>
-			</nav>
+<div class="body-wrap">
+	<div class="search">
+		<div class="search-body">
+			<form action="<?=$PHP_SELF?>" method="post">
+				<input type="text" name="search">
+				<div class="button">
+					<input type="submit" name="submit" value='Submit'>
+				</div>
+			</form>
 		</div>
-		<div class="body-wrap">
-			<div class="left-wrap">
-				<!-- site information - temp info -->
-				<li style="color:white">thing</li>
-				<li style="color:white">thing</li>
-				<li style="color:white">thing</li>
-				<li style="color:white">thing</li>
-				<li style="color:white">thing</li>
-				<li style="color:white">thing</li>
-				<li style="color:white">thing</li>
-				<li style="color:white">thing</li>
-				<li style="color:white">thing</li>
-			</div>
-			<div class="right-wrap">
-				<!-- list of users - temp info-->
+	</div>
+	<div class="results">
+		<!-- list of users - temp info-->
+		<h3>Results</h>
+			<div class="results-body">
 				<ul id="home-user-list">
-					<li>thing</li>
-					<li>thing</li>
-					<li>thing</li>
-					<li>thing</li>
-					<li>thing</li>
-					<li>thing</li>
-					<li>thing</li>
-					<li>thing</li>
-					<li>thing</li>
+					<li><a href="user1.php"><img src="./img/user1.png" alt="A picture of Mario"></a>Mario<br><br></li>
+					<li><a href="user2.php"><img src="./img/user2.png" alt="A picture of Luigi"></a>Luigi<br></li>
 				</ul>
 			</div>
 		</div>
-	</body>
-	<footer>
-		<div>
-			<p>David Sahud, Brandi Megrew, Curtis Mayott</p>
-			<p>February 22, 2015</p>
-		</div>
-	</footer>
-</html>
+	</div>
+
+	<?php
+	include 'footer.html';
+	?>
+
